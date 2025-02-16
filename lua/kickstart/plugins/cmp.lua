79aef8188a9a5152baaf2402abcfb591dda1b015
Lei Chen <lei.chen@smartx.com>
2025-02-16 12:28:07 +0800

    cmp.lua: add more cmp source

--- lua/kickstart/plugins/cmp.lua
+++ lua/kickstart/plugins/cmp.lua
@@ -34,6 +34,8 @@ return {
       --  into multiple repos for maintenance purposes.
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-cmdline',
     },
     config = function()
       -- See `:help cmp`
